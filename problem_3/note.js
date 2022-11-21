afacdbab80a2e1240d6ccd83324ff1bcacaa2517
Ridoy Asaduzzaman <89995670+Rid0y@users.noreply.github.com>
2022-11-21 12:20:01 +0000

    Add files via upload
    starting problem 3

---
+++ problem_3/note.js
@@ -0,0 +1,52 @@
+
+ rxjs.fromEvent(document.getElementById('add-btn'),'click')  
+ .subscribe(() => addNote()
+)
+
+function addNote(){
+   
+    // create elements
+    let newNote = document.createElement("li");
+    let noteText = document.createElement("textarea");
+
+    newNote.appendChild(noteText);
+
+    // adds text to paragraph tag
+    let text = document.getElementById("message").value;
+
+    noteText.innerHTML = text;
+
+    let createdNote = document.getElementById("notes");
+    createdNote.appendChild(newNote);
+
+
+    //Making delete button
+    let deletebutton = document.createElement("button");
+    deletebutton.innerHTML = "delete"
+    //deletebutton.onclick = () => {deleteNote(newNote)}
+
+    rxjs.fromEvent(deletebutton,'click')  
+    .subscribe(() => deleteNote(newNote)
+   )
+   
+    //making color
+    let colorbutton = document.getElementById("color");
+    noteText.style.backgroundColor = colorbutton.value
+   
+    newNote.appendChild(deletebutton);
+
+}
+
+
+
+function deleteNote(note){
+    note.remove();
+}
+
+
+
+/*
+<ul id=notes>
+    <li><textarea>
+</ul>
+ */
